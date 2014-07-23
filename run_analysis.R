f1419e5b5f7c20e319cedc759f2f52ff72bd98fb
Julio Bautista <julio.bautista@gmail.com>
2014-07-23 23:40:27 +0200

    fourth release

--- run_analysis.R
+++ run_analysis.R
@@ -15,8 +15,9 @@ Dat <- rbind(read.table(paste(p, "/train/X_train.txt", sep=""), colClasses="nume
              read.table(paste(p, "/test/X_test.txt", sep=""), colClasses="numeric"))
 #
 f <- scan(paste(p, "/features.txt", sep=""), what="character")[c(FALSE, TRUE)]
-ind <- grep("(mean|std)\\(\\)", f)
-names(Dat) <- sapply(f, # make.names {base} could do, but this is costumized
+ind <- grep("(mean|std)\\(\\)", f)  # we are interested in columns with
+                                    # "mean()" or "std()" in their names
+names(Dat) <- sapply(f, # make.names {base} can do it, but this is costumized
                      function(s){
                          s <- gsub("\\(\\)", "", s)
                          s <- gsub("-", "_", s)
@@ -26,13 +27,9 @@ names(Dat) <- sapply(f, # make.names {base} could do, but this is costumized
                          s
                      })
 #
-dummy <- c(scan(paste(p, "/train/subject_train.txt", sep="")),
-           scan(paste(p, "/test/subject_test.txt", sep="")))
-Subject <- factor(dummy)
-#levels(Subject) <- paste("S.", levels(Subject), sep="")
+Subject <- as.integer(c(scan(paste(p, "/train/subject_train.txt", sep="")),
+                        scan(paste(p, "/test/subject_test.txt", sep=""))))
 Dat$Subject <- Subject
-Sortcol <- as.integer(dummy)
-Dat$Sortcol <- Sortcol
 #
 Measurement <- factor(c(scan(paste(p, "/train/y_train.txt", sep="")),
                         scan(paste(p, "/test/y_test.txt", sep=""))))
@@ -42,7 +39,6 @@ Dat$Measurement <- Measurement
 #
 Ds1 <- cbind(as.data.frame(Subject), Dat[, ind])
 Ds1 <- cbind(as.data.frame(Measurement), Ds1)
-Ds1 <- cbind(as.data.frame(Sortcol), Ds1)
 #
 Ds1 <- split(Ds1, Measurement)
 #
@@ -50,17 +46,17 @@ Res <- NULL
 D1 <- data.frame()
 for(i in 1:length(Ds1)){
     d <- Ds1[[i]]
-    o <- order(d$Sortcol)
+    o <- order(d[2])
     d <- d[o, ]
     D1 <- rbind(D1, d)
     for(j in 1:30){
         m <- NULL
-        for(k in 4:ncol(d))
-            m <- c(m, mean(d[d[1] == j, k], na.rm = TRUE))
+        for(k in 3:ncol(d))
+            m <- c(m, mean(d[d[2] == j, k], na.rm = TRUE))
         Res <- rbind(Res, m)
     }
 }
-colnames(Res) <- names(D1)[4:ncol(D1)]
+colnames(Res) <- colnames(D1)[3:ncol(D1)]
 rownames(Res) <- NULL
 #
 write.csv(Res, "result.csv")

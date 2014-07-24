43f6131d73d124316c551479ce0e2d9943715c3e
Julio Bautista <julio.bautista@gmail.com>
2014-07-24 12:29:56 +0200

    Refactoring the code

--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,28 @@
-#########################################
-# Getting and Cleaning Data Analisys    #
-# Project script                        #    
-#########################################
-p <- paste(getwd(), "/", sep="")
+######################################################################
+# Getting and Cleaning Data Analisys                                 #
+# Project script                                                     #
+# deliver the required datasets: The data.frame D1 and the matrix D2 #
+######################################################################
+Dir <- paste(getwd(), "/", sep="")
 # Get the file if not already here
-if (!file.exists(paste(p, "UCI HAR Dataset", sep=""))) {
+if (!file.exists(paste(Dir, "UCI HAR Dataset", sep=""))) {
     sc <- scan("","It can take a while, continue Y/n? ")
     if(sc != "Y") stop("This terminates execution")
     
-    ptmp <- paste(p, "/tmp.zip", sep="")
+    tmp <- paste(Dir, "tmp.zip", sep="")
     # Download file, unzip and remove the temporary file
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-    download.file(fileUrl, destfile = ptmp, method = "wget")
-    unzip(ptmp)
-    file.remove(ptmp)
+    download.file(fileUrl, destfile = tmp, method = "wget")
+    unzip(tmp)
+    file.remove(tmp)
 }
 ## Reading data part
-p <- paste(p, "UCI HAR Dataset", sep="")
+Dir <- paste(Dir, "UCI HAR Dataset", sep="")
 #
-Dat <- rbind(read.table(paste(p, "/train/X_train.txt", sep=""), colClasses="numeric"),
-             read.table(paste(p, "/test/X_test.txt", sep=""), colClasses="numeric"))
+Dat <- rbind(read.table(paste(Dir, "/train/X_train.txt", sep=""), colClasses="numeric"),
+             read.table(paste(Dir, "/test/X_test.txt", sep=""), colClasses="numeric"))
 #
-f <- scan(paste(p, "/features.txt", sep=""), what="character")[c(FALSE, TRUE)]
+f <- scan(paste(Dir, "/features.txt", sep=""), what="character")[c(FALSE, TRUE)]
 selcol <- grep("(mean|std)\\(\\)", f)  # Choose up features: we are interested 
                                        # in those with "mean()" or "std()" in their names.
 # Convert features names to valid R names
@@ -36,38 +37,39 @@ names(Dat) <- sapply(f,
                          s
                      })
 # Add two new fixed variables columns: Subject & Measurement
-Subject <- as.integer(c(scan(paste(p, "/train/subject_train.txt", sep="")),
-                        scan(paste(p, "/test/subject_test.txt", sep=""))))
+Subject <- as.integer(c(scan(paste(Dir, "/train/subject_train.txt", sep="")),
+                        scan(paste(Dir, "/test/subject_test.txt", sep=""))))
 Dat$Subject <- Subject
 #
-Measurement <- factor(c(scan(paste(p, "/train/y_train.txt", sep="")),
-                        scan(paste(p, "/test/y_test.txt", sep=""))))
-levels(Measurement) <- scan(paste(p, "/activity_labels.txt", sep=""),
+Measurement <- factor(c(scan(paste(Dir, "/train/y_train.txt", sep="")),
+                        scan(paste(Dir, "/test/y_test.txt", sep=""))))
+levels(Measurement) <- scan(paste(Dir, "/activity_labels.txt", sep=""),
                             what="character")[c(FALSE, TRUE)]
 Dat$Measurement <- Measurement
 #
+# Create a subset of Dat called dat 
 # Dat[, selcol] is a subset of columns with "mean()" or "std()" in their names.
-Dat1 <- cbind(as.data.frame(Subject), Dat[, selcol])
-#
-Dat1 <- cbind(as.data.frame(Measurement), Dat1)
+# Append Subject column
+dat <- cbind(as.data.frame(Subject), Dat[, selcol])
+# Append Measurement column
+dat <- cbind(as.data.frame(Measurement), dat)
 #
 # Split by Measurement
-Dat1 <- split(Dat1, Measurement)
+dat <- split(dat, Measurement)
 #
 D2 <- NULL  # D2 is the resulting 180x66 matrix average
             # of each variable for each activity and each subject. 
 D1 <- data.frame()  # D1 is the 10299x68 dataset1
                     # partitioned by Measurement and each ordered by Subject.
-for(i in 1:length(Dat1)){
-    d <- Dat1[[i]]
+for(i in 1:length(dat)){
+    d <- dat[[i]]    # a Measurement
     o <- order(d[2]) # order by Subject this Measurement
     d <- d[o, ]
     D1 <- rbind(D1, d) # building up D1 
     for(j in 1:30){ # for each Subject in this Measurement calculate its columns means.
-        m <- NULL
-        for(k in 3:ncol(d)) # for each numeric column take the mean.
-            m <- c(m, mean(d[d[2] == j, k]))
-        D2 <- rbind(D2, m) # build up the matrix D2 with a new row
+        m <- colMeans(d[d[2] == j, 3:ncol(d)])
+        names(m) <- NULL
+        D2 <- rbind(D2, m) # feed up the matrix D2 with a new row
     }
 }
 colnames(D2) <- colnames(D1)[3:ncol(D1)]
